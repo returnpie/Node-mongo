4cd3cd1a046f00e58c7e6063221a48e72d702148
pie <returnpie@icloud.com>
2020-09-20 20:05:29 +0900

    start publishing

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 const path = require("path");
+const fs = require("fs");
+const https = require("https");
 
 const express = require("express");
 const bodyParser = require("body-parser");
@@ -11,6 +13,9 @@ const MongoDBStore = require("connect-mongodb-session")(session);
 const csrf = require("csurf");
 const flash = require("connect-flash");
 const multer = require("multer");
+const helmet = require("helmet");
+const compression = require("compression");
+const morgan = require("morgan");
 
 const errorController = require("./controllers/error");
 const User = require("./models/user");
@@ -21,6 +26,9 @@ const store = new MongoDBStore({
   collection: "sessions",
 });
 const csrfProtection = csrf();
+
+const privateKey = fs.readFileSync("./server.key");
+const certificate = fs.readFileSync("./-server.cert");
 
 const fileStorage = multer.diskStorage({
   destination: (req, file, cb) => {
@@ -49,6 +57,15 @@ app.set("views", "views");
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 const authRoutes = require("./routes/auth");
+
+const accessLogStream = fs.createWriteStream(
+  path.join(__dirname, "access.log"),
+  { flags: "a" }
+);
+
+app.use(helmet());
+app.use(compression());
+app.use(morgan("combined", { stream: accessLogStream }));
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(multer({ storage: fileStorage, fileFilter }).single("image"));
@@ -108,9 +125,12 @@ app.use((error, req, res, next) => {
 mongoose
   .connect(process.env.MONGODB)
   .then((result) => {
-    app.listen(process.env.PORT, () => {
-      console.log(`listen on port ${process.env.PORT}`);
-    });
+    // https
+    //   .createServer({ key: privateKey, cert: certificate }, app)
+    //   .listen(process.env.PORT, () => {
+    //     console.log(`listen on port ${process.env.PORT}`);
+    //   });
+    app.listen(process.env.PORT);
   })
   .catch((err) => {
     console.log(err);
